18df8058ca708ba841272e57e9c1bd37bc3ce675
Karan Singh Tomar <karan.tomar_cs19@gla.ac.in>
2022-11-18 19:38:33 +0530

    update

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -45,15 +45,15 @@ export const MyNavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/my-portfolio" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="linkedin.com/in/karan-singh-tomar-96b6401b9/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/karansingh.tomar.351/"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/karansingh.tomar.351/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span><nobr>Let’s Connect</nobr></span></button>
